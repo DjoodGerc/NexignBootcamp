0b3f583f94268b82779767f583c9a2251c173497
Avramenko_Pavel <avramenko.pav@yandex.ru>
2025-03-20 20:05:13 +0300

    init commit

---
+++ src/main/java/com/example/demo/NexignBootcampTestCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexignBootcampTestCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexignBootcampTestCaseApplication.class, args);
+	}
+
+}
